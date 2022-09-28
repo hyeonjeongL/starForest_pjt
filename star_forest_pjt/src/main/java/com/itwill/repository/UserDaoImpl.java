cc9e11256921ccb22274b58aade7bca18e778da7
yejihan <aaa8392@naver.com>
2022-09-28 11:52:17 +0900

    *

--- star_forest_pjt/src/main/java/com/itwill/repository/UserDaoImpl.java
+++ star_forest_pjt/src/main/java/com/itwill/repository/UserDaoImpl.java
@@ -1,10 +1,11 @@
 package com.itwill.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public class UserDaoImpl {
-
+public class UserDaoImpl implements UserDao {
+	
 }

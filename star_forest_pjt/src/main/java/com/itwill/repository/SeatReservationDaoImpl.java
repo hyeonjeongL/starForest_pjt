1af0b22a6042942d877052b6282ecec86db2f556
ddoyoon <ddoyoon@localhost>
2022-09-28 16:31:39 +0900

    ..

--- star_forest_pjt/src/main/java/com/itwill/repository/SeatReservationDaoImpl.java
+++ star_forest_pjt/src/main/java/com/itwill/repository/SeatReservationDaoImpl.java
@@ -1,10 +1,63 @@
 package com.itwill.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Mapper
+import com.itwill.domain.SeatReservation;
+import com.itwill.mapper.SeatReservationMapper;
+
 @Repository
-public class SeatReservationDaoImpl {
+public class SeatReservationDaoImpl implements SeatReservationDao{
+	@Autowired
+	private SeatReservationMapper seatReservationMapper;
+
+	public SeatReservationDaoImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public int reservation(SeatReservation seatReservation) throws Exception {
+		return seatReservationMapper.reservation();
+	}
+
+	@Override
+	public int returnByUser(SeatReservation seatReservation) throws Exception {
+		return seatReservationMapper.returnByUser();
+	}
+
+	@Override
+	public int returnByAuto(SeatReservation seatReservation) throws Exception {
+		return seatReservationMapper.returnByAuto();
+	}
+
+	@Override
+	public int continueSeat(SeatReservation seatReservation) throws Exception {
+		return seatReservationMapper.continueSeat();
+	}
+
+	@Override
+	public List<SeatReservation> selectAll() throws Exception {
+		return seatReservationMapper.selectAll();
+	}
+
+	@Override
+	public List<SeatReservation> selectAvailableSeat(int seat_status) throws Exception {
+		return seatReservationMapper.selectAvailableSeat(seat_status);
+	}
+
+	@Override
+	public int countAvailableSeat(int seat_status) throws Exception {
+		return seatReservationMapper.countAvailableSeat(seat_status);
+	}
+
+	@Override
+	public SeatReservation selectTimeUsingSeat(String seat_no, int seat_status) throws Exception {
+		return seatReservationMapper.selectTimeUsingSeat(seat_no, seat_status);
+	}
+
+	
 
 }

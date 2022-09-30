eff6d0fd58539479013a670cda5c50f6c4570db6
hunnnnii94 <user@DESKTOP-GNQOK6U>
2022-09-30 16:15:54 +0900

    testing

--- star_forest_pjt/src/main/java/com/itwill/controller/test/BookDaoTestController.java
+++ star_forest_pjt/src/main/java/com/itwill/controller/test/BookDaoTestController.java
@@ -1,51 +1,34 @@
 package com.itwill.controller.test;
 
-import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.itwill.domain.Book;
-import com.itwill.domain.Rental;
+import com.itwill.mapper.BookMapper;
+import com.itwill.mapper.NoticeMapper;
 import com.itwill.repository.BookDao;
-import com.itwill.repository.RentalDao;
+import com.itwill.repository.NoticeDao;
 
-@Controller
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.itwill" }, includeFilters = {
+		@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { BookDao.class, BookMapper.class }) })
 public class BookDaoTestController {
 
-	@Autowired
-	private BookDao bookDao;
-	
-	private RentalDao rentalDao;
-	
-	
-	//book 
-	@RequestMapping("/book_test")
-	public String book_dao_test() throws Exception {
-		
-		Book testBook = new Book(0, 502173640,"임현정","스프링부트를찾아서", "아이티윌","한달의 프로젝트기간 ....", "2022년 9월", null, null, null, 200, 3, 0, 0, 100);
-		System.out.println("book dao test");
-		System.out.println(bookDao.selectBookDetail(12));
-		System.out.println(bookDao.updateRentalBookQty(1));
-		System.out.println(bookDao.updateReturnBookQty(2));
-		System.out.println(bookDao.updateById("hyeonjeong"));
-		System.out.println(bookDao.updateByIdNo("yeji",3));
-		System.out.println(bookDao.selectAll());
-		
-		return "test/test_result";
+	public static void main(String[] args) throws Exception {
+		SpringApplication application = new SpringApplication(BookDaoTestController.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		ConfigurableApplicationContext context = application.run(args);
+		BookDao bookDao = (BookDao) context.getBean(BookDao.class);
+//		System.out.println(bookDao.selectBookDetail(12));
+// 		System.out.println(bookDao.updateRentalBookQty(1));
+// 		System.out.println(bookDao.updateReturnBookQty(2));
+		System.out.println(bookDao.updateById("hyeonjeong")); //왜 한권빌린애는 처리되는데 3권빌린 나는 처리 안되지 밀당오지네 완료
+//		System.out.println(bookDao.updateByIdNo("yeji",2)); //맵만 해결하면 완료
+//		System.out.println(bookDao.selectAll());
 	}
-	
-	public String rental_dao_test() {
-		
-		System.out.println(rentalDao.updateDate("hyeonjeong", 4));
-		System.out.println(rentalDao.selectById("hyeonjeong"));
-		System.out.println(rentalDao.selectByNo(2));
-		System.out.println(rentalDao.updateRentalStatus("hyeonjeong", 2));
-		
-		return  "test/test_result";
-		
-	}
-	
-	
+
 }
--- star_forest_pjt/src/main/java/com/itwill/mapper/ReservationMapper.java
+++ star_forest_pjt/src/main/java/com/itwill/mapper/ReservationMapper.java
@@ -1,5 +1,24 @@
 package com.itwill.mapper;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwill.domain.Reservation;
+
+@Mapper
 public interface ReservationMapper {
+	
+	//책 수량이 0일 때 예약 리스트 추가하기
+	@Insert("insert into reservation "
+		  + "values (seq_reservation_res_no.nextval,sysdate, 1, 9, 'yeji')")
+	public int insertReservation(Reservation reservation);
+	
+	
+	//예약이 대출로 변경되면 예약리스트 삭제
+	@Delete("delete from reservation where user_id=#{user_id} and book_no=#{book_no}")
+	public int deleteReservation(String user_id, int book_no);
+	
+	
 
 }

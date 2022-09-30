0184f80ed29c6e479dd2abe90f7ef7d557eaeebd
ddoyoon <1026jsy@gmail.com>
2022-09-30 13:21:17 +0900

    ..

--- star_forest_pjt/src/main/java/com/itwill/controller/test/SoyoonDaoTestController.java
+++ star_forest_pjt/src/main/java/com/itwill/controller/test/SoyoonDaoTestController.java
@@ -1,32 +1,44 @@
 package com.itwill.controller.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.domain.SeatReservation;
+import com.itwill.mapper.NoticeMapper;
+import com.itwill.repository.NoticeDao;
 import com.itwill.repository.SeatReservationDao;
 import com.itwill.service.SeatReservationService;
 
-@Controller
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.itwill"}, includeFilters = {
+        @Filter(
+                type = FilterType.ASSIGNABLE_TYPE, 
+                classes = {NoticeDao.class,NoticeMapper.class}
+            )
+        } )
 public class SoyoonDaoTestController {
-	@Autowired
-	private SeatReservationDao seatReservationDao;
-	@Autowired
-	private SeatReservationService seatReservationService;
-	
-	@RequestMapping("/seatReservationDao_test")
-	public String seatReservationDao_test() throws Exception{
 
-		//System.out.println(seatReservationDao.selectAll());
-		//System.out.println(seatReservationDao.reservation(new SeatReservation("A01", null, null, null, 0, "soyoon")));
-		SeatReservation usingSeat = seatReservationDao.selectTimeUsingSeat("B01", 1);
-		
-		//System.out.println(usingSeat);
-		//System.out.println(seatReservationDao.returnByAuto(usingSeat));
-		//System.out.println(seatReservationDao.continueSeat(usingSeat));
-		System.out.println(seatReservationDao.countAvailableSeat(0));
-		return "test/test_result";
+   public static void main(String[] args) throws Exception{
+      SpringApplication application = 
+            new SpringApplication(Noticetest1234.class);
+      application.setWebApplicationType(WebApplicationType.NONE);
+      ConfigurableApplicationContext context=application.run(args);
+      SeatReservationDao seatReservationDao=(SeatReservationDao)context.getBean(SeatReservationDao.class);
+      System.out.println(seatReservationDao.selectAll());
+      System.out.println(seatReservationDao.countAvailableSeat(0));
+      System.out.println(seatReservationDao.selectAvailableSeat(0));
+      SeatReservation usingSeat = seatReservationDao.selectTimeUsingSeat("B01", 1);
+      System.out.println(seatReservationDao.returnByAuto(usingSeat));
+      
+      //좌석 테스트 완료
+   }
 
-	}
 }

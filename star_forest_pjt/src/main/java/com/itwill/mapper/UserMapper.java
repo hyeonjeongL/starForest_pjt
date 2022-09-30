c685289c6ea38756fb61b1a8df0029400d5f5cdc
yejihan <aaa8392@naver.com>
2022-09-30 09:05:03 +0900

    user

--- star_forest_pjt/src/main/java/com/itwill/mapper/UserMapper.java
+++ star_forest_pjt/src/main/java/com/itwill/mapper/UserMapper.java
@@ -1,5 +1,44 @@
 package com.itwill.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.itwill.domain.User;
+
+@Mapper
 public interface UserMapper {
 
+	@Insert("insert into user_info values(#{user_id},#{user_name},#{user_password},#{user_email},"
+			+ "									 #{user_birth},#{user_gender},#{user_phone},#{user_address},"
+			+ "									 #{user_qr},#{user_rental_status},#{user_book_cnt_limit},"
+			+ "									 #{user_book_weight},#{category_no})")
+	public int create(User user);
+
+	@Update("update user_info set user_password=#{user_password} where user_id=#{user_id}")
+	public int updatePassword(User user);
+
+	@Update("update user_info set user_password=#{user_password},user_email=#{user_email},user_birth=#{user_birth},"
+			+ "		user_phone=#{user_phone},user_address=#{user_address},category_no=#{category_no} where user_id=#{user_id}")
+	public int update(User user);
+
+	@Delete("delete from user_info where user_id=#{user_id}")
+	public int remove(String user_id);
+
+	@Select("select * from user_info")
+	public List<User> userList();
+
+	@Select("select * from user_info where user_id=#{user_id}")
+	public User findUser(String user_id);
+
+	@Select("select count(*) from user_info where user_id=#{user_id}")
+	public boolean existedUser(String user_id);
+
+	@Update("update user_info set user_book_cnt_limit=user_book_cnt_limit-1"
+			+ "		where user_book_cnt_limit>0 and user_id=#{user_id}")
+	public int userRentalCount(String user_id);
 }
---
+++ star_forest_pjt/산출물/user_info.sql
@@ -0,0 +1,56 @@
+/***********User***********
+이름                  널?       유형            
+------------------- -------- ------------- 
+USER_ID             NOT NULL VARCHAR2(100) 
+USER_NAME                    VARCHAR2(50)  
+USER_PASSWORD                VARCHAR2(20)  
+USER_EMAIL                   VARCHAR2(100) 
+USER_BIRTH                   DATE          
+USER_GENDER                  VARCHAR2(50)  
+USER_PHONE                   VARCHAR2(30)  
+USER_ADDRESS                 VARCHAR2(50)  
+USER_QR                      VARCHAR2(50)  
+USER_RENTAL_STATUS           VARCHAR2(100) 
+USER_BOOK_CNT_LIMIT          NUMBER        
+USER_BOOK_WEIGHT             NUMBER        
+CATEGORY_NO         NOT NULL NUMBER      
+*/
+
+desc user_info;
+desc book_category;
+--category--
+insert into book_category values(100,'건강/취미/레저');
+insert into book_category values(200,'경제경영');
+insert into book_category values(300,'고전');
+insert into book_category values(400,'과학');
+insert into book_category values(500,'만화');
+insert into book_category values(600,'사회과학');
+insert into book_category values(700,'소설/시/희곡');
+
+--user_info insert--
+insert into user_info values('yeji','한예지','1111','yeji@naver.com','1995-01-30','F','01012345678','경기도 김포시',null,'대출가능',3,15,100);
+insert into user_info values('soyoon','장소윤','2222','soyun@naver.com','1994-08-04','F','01012345678','경기도 김포시',null,'대출가능',3,80,200);
+insert into user_info values('hyeonjeong','임현정','3333','hyeonjeong@naver.com','1994-06-12','F','01012345678','경기도 김포시',null,'대출가능',3,56,300);
+insert into user_info values('junghyun','이정현','4444','jeonghyun@naver.com','1996-08-09','F','01012345678','경기도 김포시',null,'대출가능',3,45,400);
+insert into user_info values('woohyuk','이우혁','5555','woohyuk@naver.com','1997-03-23','F','01012345678','경기도 김포시',null,'대출가능',3,88,600);
+insert into user_info values('admin','관리자','0000','admin@naver.com','1980-03-23','M','01012345678','경기도 김포시',null,null,0,0,0);
+
+--select--
+/*모든 회원의 정보 출력*/
+select * from user_info;
+/*'yeji'회원의 정보 출력*/
+select * from user_info where user_id='yeji';
+/*아이디 중복체크*/
+select count(*) from user_info where user_id='soyun';
+
+--update--
+/*회원 비밀번호변경*/
+update user_info set user_password='1234' where user_id='yeji';
+/*'yeji' 회원 정보 수정 - 이름,성별,아이디,qr,대출가능여부 변경 불가 */
+update user_info set user_password='0000',user_email='han@naver.com',user_birth='1995-01-01',user_phone='01022223333',user_address='경기도 부천시',category_no=200 where user_id='soyun';
+/*yeji 대출권수 카운트*/
+update user_info set user_book_cnt_limit=user_book_cnt_limit-1 where user_book_cnt_limit>0 and user_id='soyun';
+
+--delete--
+/*회원탈퇴*/
+delete from user_info where user_id='yeji';

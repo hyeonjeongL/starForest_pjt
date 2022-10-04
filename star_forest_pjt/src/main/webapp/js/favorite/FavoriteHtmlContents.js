70afc356751d7f06b4ad62b8d4b82f3a2d626a58
yejihan <aaa8392@naver.com>
2022-10-04 11:07:58 +0900

    *

--- star_forest_pjt/src/main/webapp/js/favorite/FavoriteHtmlContents.js
+++ star_forest_pjt/src/main/webapp/js/favorite/FavoriteHtmlContents.js
@@ -1,6 +1,30 @@
-function FavoriteHtmlContents(){
+function FavoriteHtmlContents() {
 }
-FavoriteHtmlContents.favorite_list_content=function(){
+FavoriteHtmlContents.favorite_item_content = function(favorite) {
+	return `<div class="e-item d-md-flex align-items-center">
+					<div class="item-no">${favorite.favorite_no}</div>
+						<div class="item item-data d-flex">
+							<div class="item-cover">
+								 <a href="/detail/?cid=CAT000000949038&ctype=m"></a>
+							</div>
+								<div class="item-meta">
+									<div class="item-isbn">
+										${favorite.book.isbn}																			</div>
+									<div class="item-title">
+										<h4><a href="/detail/?cid=CAT000000949038&ctype=m">${favorite.book.book_title}</a></h4>
+									</div>
+										<div class="item-author">${favorite.book.book_author}</div>
+										<div class="item-pub">${favorite.book.book_publisher}</div>
+									</div>								
+								</div>
+						<div class="item-functions">
+							<a role="button" class="btn btn-sm btn-primary item-delete-trigger" item-val="234279">삭제</a>
+					</div>
+			</div><!-- item : 1 -->
+`;
+}
+
+FavoriteHtmlContents.favorite_list_content = function(favoriteArray) {
 	return `
 <div class="entry-content">
 	
@@ -12,44 +36,26 @@ FavoriteHtmlContents.favorite_list_content=function(){
 					<div class="items-header directory-header d-flex justify-content-lg-between align-items-baseline">
 						<h4 class="selected-dir-name">전체</h4>
 						<div class="item-count">
-							<span class="number">2</span>건						</div>
+							<span class="number">${favorite.length}</span>건						</div>
 					</div><!-- .items-header -->
 					
 					<div class="e-items list">
 						
 						<div class="e-item item-headings d-none d-xl-flex align-items-center">
 							<div class="item-index">번호</div>
-							<div class="item-data">자료</div>
-							<div class="item-date">등록일</div>
+							<div class="item-data">ISBN</div>
+							<div class="item-data">제목</div>
+							<div class="item-data">저자</div>
+							<div class="item-data">출판사</div>
 							<div class="item-functions">작업</div>
 						</div><!-- .item-headings -->
 
 												
-						<div class="e-item d-md-flex align-items-center">
-							<div class="item-index">1</div>
-							<div class="item item-data d-flex">
-								<div class="item-cover">
-									<a href="/detail/?cid=CAT000000949038&ctype=m">
-																													<div class="item-cover-image" style="background-image:url(/n2app/public/coverimage.php?sysd=CAT&control_no=000000949038)"></div>
-																		</a>
-								</div>
-								<div class="item-meta">
-									<div class="item-type">
-										단행본																			</div>
-									<div class="item-title">
-										<h4><a href="/detail/?cid=CAT000000949038&ctype=m">자바를 자바라 (48회 대출)</a></h4>
-									</div>
-									<div class="item-author">신정호</div>
-									<div class="item-pub">미래정보사 (1996)</div>
-								</div>								
-							</div>
-							<div class="item-date"><span class="d-inline d-lg-none">등록일 </span>2022-10-04</div>
-							<div class="item-functions">
-								<a role="button" class="btn btn-sm btn-primary item-delete-trigger" item-val="234279">삭제</a>
-								<a role="button" class="btn btn-sm btn-primary set-item-val" item-val="234279" data-toggle="modal" data-target="#directory-select">이동</a>
-							</div>
-						</div><!-- item : 1 -->
-
+						<!--favorite start-->
+						${
+							favoriteArray.map(favorite_item_content).join('')							
+						}
+						<!--favorite end-->
 												
 					</div><!-- .e-items.list -->
 			
@@ -61,7 +67,7 @@ FavoriteHtmlContents.favorite_list_content=function(){
 	
 </div><!-- .entry-content -->
 `;
-	
+
 }
 
 

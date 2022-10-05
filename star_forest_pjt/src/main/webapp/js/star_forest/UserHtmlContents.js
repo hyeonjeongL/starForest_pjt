3606a0a8d1a9605639545624133bfcb9944bdfe6
yejihan <aaa8392@naver.com>
2022-10-05 17:16:01 +0900

    *

--- star_forest_pjt/src/main/webapp/js/star_forest/UserHtmlContents.js
+++ star_forest_pjt/src/main/webapp/js/star_forest/UserHtmlContents.js
@@ -1,5 +1,20 @@
 function UserHtmlContents(){
 }
-UserHtmlContents.user_
+UserHtmlContents.user_login_content=function(){
+	return `
+	`;
+}
+UserHtmlContents.user_logout_content=function(){
+	return `
+	`;
+}
+UserHtmlContents.user_login_form_content=function(){
+	return `
+	`;	
+}
+UserHtmlContents.user_write_form_content=function(){
+	return `
+	`;
+}
 
 

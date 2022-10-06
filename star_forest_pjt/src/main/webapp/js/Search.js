0af9e3b2ebe4b495f368fc8c97f9ce084e51dc5f
academyitwill <jhyun9682@naver.com>
2022-10-06 17:14:56 +0900

    검쉙+가입

--- star_forest_pjt/src/main/webapp/js/Search.js
+++ star_forest_pjt/src/main/webapp/js/Search.js
@@ -1,26 +1,4 @@
-function getSearchList(){
-	$.ajax({
-		type: 'GET',
-		url : "/getSearchList",
-		data : $("form[name=search-form]").serialize(),
-		success : function(result){
-			//테이블 초기화
-			$('#boardtable > tbody').empty();
-			if(result.length>=1){
-				result.forEach(function(item){
-					str='<tr>'
-					str += "<td>"+item.idx+"</td>";
-					str+="<td>"+item.writer+"</td>";
-					str+="<td><a href = '/board/detail?idx=" + item.idx + "'>" + item.title + "</a></td>";
-					str+="<td>"+item.date+"</td>";
-					str+="<td>"+item.hit+"</td>";
-					str+="</tr>"
-					$('#boardtable').append(str);
-        		})				 
-			}
-		}
-	})
-}
+
 
 
 function keywordCheck() {
@@ -32,3 +10,18 @@ function keywordCheck() {
 		}
 		window.searchform.submit();
 	}
+	
+	
+	/* 검색 */
+$(function() {
+	$('#search').click(function() {
+		location.href = "http://localhost/star_forest_pjt/SearchList?query=" + $("#book_title").val();
+	})
+});
+
+/* 추천도서 누르면 bookDetail 페이지로 이동 */
+$(function() {
+	$('.card').click(function() {
+		location.href = "http://localhost/star_forest_pjt/detailBook.?b_no=1&query=" + $(this).find('#book_title').text();
+	})
+});

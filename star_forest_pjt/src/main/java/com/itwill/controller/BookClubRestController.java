74368707b0b1c698a9628101cb00eb5802409ddc
ddoyoon <ddoyoon@localhost>
2022-10-15 02:17:13 +0900

    ..

--- star_forest_pjt/src/main/java/com/itwill/controller/BookClubRestController.java
+++ star_forest_pjt/src/main/java/com/itwill/controller/BookClubRestController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itwill.controller.interceptor.LoginCheck;
 import com.itwill.domain.BookClub;
 import com.itwill.service.BookClubService;
+import com.itwill.util.Criteria;
 
 @RestController
 public class BookClubRestController {
